c7df5ebd6669c1937cc8a92c6e099b3e6b06a7f0
davisch01 <140275848+davisch01@users.noreply.github.com>
2023-07-22 21:30:01 -0500

    sketch out service and tests

--- CSharp/Services/OrdersService.cs
+++ CSharp/Services/OrdersService.cs
@@ -2,30 +2,100 @@
 using CSharp.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharp.Services
 {
     public class OrdersService : IOrdersService
     {
-
-        public OrderModel CancelOrder(Guid orderId)
+        //later replaced by repository services
+        private readonly List<OrderModel> _repository = new List<OrderModel>
         {
-            throw new NotImplementedException();
+            new OrderModel{
+                CustomerId = new Guid("85fb3113-c1c2-4a09-9eb3-f87c41d86709"),
+                OrderId = new Guid("b0cc525d-485b-41d5-809a-2b385e64d97e"),
+                Status = OrderStatus.Active,
+                Items = new List<string>{"Pizza"}
+            },
+            new OrderModel{
+                CustomerId = new Guid("85fb3113-c1c2-4a09-9eb3-f87c41d86709"),
+                OrderId = new Guid("50dc750b-65da-4906-b081-fe989e868bce"),
+                Status = OrderStatus.Canceled,
+                Items = new List<string>{"Pie"}
+            },
+            new OrderModel{
+                CustomerId = new Guid("85fb3113-c1c2-4a09-9eb3-f87c41d86709"),
+                OrderId = new Guid("7afb484e-4a30-4bdf-907c-cc5e7f141449"),
+                Status = OrderStatus.Active,
+                Items = new List<string>{"Panini"}
+            },
+            new OrderModel{
+                CustomerId = new Guid("2f5a2039-ea51-41a4-8bd2-14ff67618c35"),
+                OrderId = new Guid("2ad32e29-d978-4e96-b400-68cb48b6fb11"),
+                Status = OrderStatus.Active,
+                Items = new List<string>{"Pizza"}
+            },
+            new OrderModel{
+                CustomerId = new Guid("2f5a2039-ea51-41a4-8bd2-14ff67618c35"),
+                OrderId = new Guid("2a5cc20f-518f-469a-bea9-bbfeb55d4af9"),
+                Status = OrderStatus.Active,
+                Items = new List<string>{"Pizza"}
+            },
+            new OrderModel{
+                CustomerId = new Guid("2f5a2039-ea51-41a4-8bd2-14ff67618c35"),
+                OrderId = new Guid("43dc83d9-4aa2-4f32-8f13-ef1eb2fe442f"),
+                Status = OrderStatus.Active,
+                Items = new List<string>{"Pizza"}
+            }
+        };
+
+        public List<OrderModel> GetOrdersByCustomerId(Guid customerId)
+        {
+            return _repository.Where(i => i.CustomerId == customerId).ToList();
         }
 
         public OrderModel CreateOrder(OrderModel order)
         {
-            throw new NotImplementedException();
-        }
+            //TODO validations
+            var addedOrderModel = new OrderModel
+            {
+                CustomerId = order.CustomerId,
+                OrderId = new Guid(),
+                Status = OrderStatus.Active,
+                Items = order.Items
+            };
 
-        public List<OrderModel> GetOrdersByCustomerId(Guid customerId)
-        {
-            throw new NotImplementedException();
+            _repository.Add(addedOrderModel);
+            return addedOrderModel;
         }
 
         public OrderModel UpdateOrder(OrderModel order)
         {
-            throw new NotImplementedException();
+            //TODO validations
+            var existingOrder = _repository.Where(i => i.OrderId == order.OrderId).FirstOrDefault();
+
+            if (existingOrder == null)
+            {
+                return null;
+            }
+
+            existingOrder.Items = order.Items;
+
+            return existingOrder;
+        }
+
+        public OrderModel CancelOrder(Guid orderId)
+        {
+            var existingOrder = _repository.Where(i => i.OrderId == orderId).FirstOrDefault();
+
+            if (existingOrder == null)
+            {
+                return null;
+            }
+
+            existingOrder.Status = OrderStatus.Canceled;
+
+            return existingOrder;
         }
     }
 }
